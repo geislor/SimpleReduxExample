a7e62176b8074dd54d3decb07d79b970afc293a2
crestani <geislor@gmail.com>
2018-04-01 08:17:13 -0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 87 },
+        { title: 'The Dark Tower', pages: 38 },
+        { title: 'Eloquent Ruby', pages: 1 }
+    ]
+}
